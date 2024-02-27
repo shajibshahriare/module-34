d4476a4ae44c06c779e19d99d69546a39a3675a4
shajibshahriare <shajib.shahriare16@gmail.com>
2024-02-28 02:32:31 +0600

    modal section added with html and js

--- scripts/phone.js
+++ scripts/phone.js
@@ -21,12 +21,11 @@ const displayPhones = (phones, isShowAll) => {
         showAllBtn.classList.add('hidden')
     }
 
-    if (!isShowAll){
+    if (!isShowAll) {
         phones = phones.slice(0, 12)
     }
 
     phones.forEach(phone => {
-        console.log(phone)
         const phoneCard = document.createElement('div')
         phoneCard.classList = `card p-4 bg-base-100 shadow-xl`
         phoneCard.innerHTML = `
@@ -34,8 +33,8 @@ const displayPhones = (phones, isShowAll) => {
      <div class="card-body">
        <h2 class="card-title">${phone.phone_name}</h2>
        <p>If a dog chews shoes whose shoes does he choose?</p>
-       <div class="card-actions justify-end">
-         <button class="btn btn-primary">Buy Now</button>
+       <div class="card-actions justify-center">
+         <button onclick ="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
        </div>
      </div>
      
@@ -46,6 +45,39 @@ const displayPhones = (phones, isShowAll) => {
     });
 
     toggleLoadingSpinner(false)
+
+}
+
+const handleShowDetails = async (id) => {
+    // console.log(id)
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json()
+
+    const phone = data.data
+    showPhoneDetails(phone)
+}
+
+const showPhoneDetails = (phone) => {
+    console.log(phone)
+    const phoneName = document.getElementById('phone-name')
+    phoneName.innerText = phone.name;
+
+    const showDetailContainer = document.getElementById('show-detail-container')
+    showDetailContainer.innerHTML = `
+    <img src="${phone.image}" alt="">
+    <p> <span>Storage:</span>${phone?.mainFeatures?.storage}</p>
+    <p> <span>Chipset:</span>${phone?.mainFeatures?.displaySize}</p>
+    <p> <span>Memory:</span>${phone?.mainFeatures?.memory}</p>
+    <p> <span>Slug:</span>${phone?.slug}</p>
+    <p> <span>Release Date:</span>${phone?.releaseDate}</p>
+    <p> <span>Brand:</span>${phone?.brand}</p>
+    <p> <span>GPS:</span>${phone?.others?.GPS}</p>
+    
+    
+    
+    `
+
+    show_details_modal.showModal()
 
 }
 
@@ -61,13 +93,13 @@ const handleSearchBtn = (isShowAll) => {
 const toggleLoadingSpinner = (isLoading) => {
     const loadingSpinner = document.getElementById('loading-spinner');
 
-    if(isLoading){
+    if (isLoading) {
         loadingSpinner.classList.remove('hidden')
     }
-    else{
+    else {
         loadingSpinner.classList.add('hidden')
     }
-    
+
 }
 
 const handleShowAll = () => {
