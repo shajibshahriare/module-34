2cdb65442638c7bff7f1a96826ffd87a4f2d2720
shajibshahriare <shajib.shahriare16@gmail.com>
2024-02-27 23:52:49 +0600

    basic html js added

---
+++ scripts/phone.js
@@ -0,0 +1,42 @@
+const loadPhone = async (searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+    const data = await res.json()
+    const phones = data.data
+    displayPhones(phones)
+}
+
+
+const displayPhones = phones => {
+
+    const phoneContainer = document.getElementById('phone-container')
+    phoneContainer.textContent = ''
+    phones.forEach(phone => {
+        console.log(phone)
+        const phoneCard = document.createElement('div')
+        phoneCard.classList = `card p-4 bg-base-100 shadow-xl`
+        phoneCard.innerHTML = `
+     <figure><img src="${phone.image}" alt="Shoes" /></figure>
+     <div class="card-body">
+       <h2 class="card-title">${phone.phone_name}</h2>
+       <p>If a dog chews shoes whose shoes does he choose?</p>
+       <div class="card-actions justify-end">
+         <button class="btn btn-primary">Buy Now</button>
+       </div>
+     </div>
+     
+     
+     
+     `;
+        phoneContainer.appendChild(phoneCard)
+    });
+}
+
+const handleSearchBtn = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value
+    loadPhone(searchText)
+
+}
+
+
+// loadPhone()
